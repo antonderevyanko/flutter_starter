ad85bba45a42833fc852cc7f3b02183d4097c913
Anton Derevyanko <dea@ciklum.com>
2023-07-21 15:50:39 +0300

    Localization was introduced into the project

--- starter_app/lib/main.dart
+++ starter_app/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:feature_example/feature_example.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:starter/injection/injection.dart' as injection;
+import 'package:starter/localization/localization.dart';
 
 void main() {
   injection.configureInjection();
@@ -13,7 +16,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: 'Flutter Demo',
+        title: StarterLocalizations.of(context).app_title,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          DefaultCupertinoLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          StarterLocalizations.delegate,
+        ],
+        supportedLocales: StarterLocalizations.supportedLocales,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
